8a040ce8e81e2f1dbbadd76496e4b86defca8c6e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-04-23 19:29:33 -0500

    style: 🎨 Run `rome`

--- api/controllers/admin.controllers.ts
+++ api/controllers/admin.controllers.ts
@@ -1,21 +1,38 @@
+import { encryptPassword } from '../libs/helpers_bcrypt.js';
 import { getResidents } from '../models/admin.models.js';
-import { Request, Response } from 'express';
 import { GetUserByIdentification } from '../models/users.model.js';
 import { RegisterAdmin } from '../models/users.model.js';
-import { encryptPassword } from '../libs/helpers_bcrypt.js';
+import { Request, Response } from 'express';
 
 export const handleGetResidents = async (_req: Request, res: Response) => {
 	try {
-    const [error, residents] = await getResidents();
+		const [error, residents] = await getResidents();
 
-    if(error){
-      return res.status(500).json({ error: true, message: "Internal server error fetching the residents. Try again later." });
-    }
+		if (error) {
+			return res
+				.status(500)
+				.json({
+					error: true,
+					message:
+						'Internal server error fetching the residents. Try again later.',
+				});
+		}
 
-    return res.status(200).json({ error: false, message: "Residents fetched successfully.", residents});
-  } catch (error) {
-    return res.status(500).json({ error: true, message: "Internal Server Error. Try again later." });
-  }
+		return res
+			.status(200)
+			.json({
+				error: false,
+				message: 'Residents fetched successfully.',
+				residents,
+			});
+	} catch (_error) {
+		return res
+			.status(500)
+			.json({
+				error: true,
+				message: 'Internal Server Error. Try again later.',
+			});
+	}
 };
 
 export const handleStaffSignup = async (req: Request, res: Response) => {
@@ -24,47 +41,70 @@ export const handleStaffSignup = async (req: Request, res: Response) => {
 
 		// Check the role is valid
 		if (role !== 1 && role !== 2) {
-			return res.status(400).json({ error: true, message: "Invalid role." });
+			return res.status(400).json({ error: true, message: 'Invalid role.' });
 		}
 
-    // Check if the user already exists
-    const identificationExists = await GetUserByIdentification(identification_card, role);
-    if (identificationExists) {
-      return res.status(400).json({ error: true, message: "There is another user with the given identification card" });
-    }
+		// Check if the user already exists
+		const identificationExists = await GetUserByIdentification(
+			identification_card,
+			role,
+		);
+		if (identificationExists) {
+			return res
+				.status(400)
+				.json({
+					error: true,
+					message: 'There is another user with the given identification card',
+				});
+		}
 
-    // Check if the mail already exists
-    const mailExists = await GetUserByIdentification(mail, role);
-    if (mailExists) {
-      return res.status(400).json({ error: true, message: "There is another user with the given mail" });
-    }
+		// Check if the mail already exists
+		const mailExists = await GetUserByIdentification(mail, role);
+		if (mailExists) {
+			return res
+				.status(400)
+				.json({
+					error: true,
+					message: 'There is another user with the given mail',
+				});
+		}
 
-    // Hash the password
-    const [err, hash] = await encryptPassword(password);
-    if (err) {
-      return res.status(500).json({ error: true, message: "Internal Server Error. Try again later." });
-    }
+		// Hash the password
+		const [err, hash] = await encryptPassword(password);
+		if (err) {
+			return res
+				.status(500)
+				.json({
+					error: true,
+					message: 'Internal Server Error. Try again later.',
+				});
+		}
 
-    // Create the user
-    const wasAdminCreated = await RegisterAdmin({
-      identification_card,
-      name,
-      mail,
-      role,
-      "password": hash,
-    })
+		// Create the user
+		const wasAdminCreated = await RegisterAdmin({
+			identification_card,
+			name,
+			mail,
+			role,
+			'password': hash,
+		});
 
-    if (!wasAdminCreated) {
-      return res.status(500).json({ error: true, message: "Internal Server Error. Try again later." });
-    }
+		if (!wasAdminCreated) {
+			return res
+				.status(500)
+				.json({
+					error: true,
+					message: 'Internal Server Error. Try again later.',
+				});
+		}
 
-    return res.status(201).json({ error: false, message: "Admin created successfully." });
-	} catch (_error) {
 		return res
-			.status(500)
-			.json({
-				error: true,
-				message: "Internal Server Error. Try again later.",
-			});
+			.status(201)
+			.json({ error: false, message: 'Admin created successfully.' });
+	} catch (_error) {
+		return res.status(500).json({
+			error: true,
+			message: 'Internal Server Error. Try again later.',
+		});
 	}
 };
